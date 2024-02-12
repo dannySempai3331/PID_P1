eb42876824d0eecdcba967c0a01a2af1347108fc
jdaniel31 <jdaniel10031@outlook.com>
2024-02-12 15:41:18 -0600

    listo

--- gui.py
+++ gui.py
@@ -7,23 +7,28 @@ img = Image.open('Paisaje.jpg')
 
 def getCoordenadas():
 
-    #verificar si el panel de la imagen tiene una imagen
     if panel_imagen.winfo_ismapped():
-
-        #verificar si los campos de las coordenadas estan vacios
 
         if entryX1.get() == "" or entryY1.get() == "" or entryX2.get() == "" or entryY2.get() == "":
             messagebox.showerror("Error", "¡Coordenadas no validas!") 
+
         else:
+
             C1 = (int(entryX1.get()), int(entryY1.get()))
             C2 = (int(entryX2.get()), int(entryY2.get()))
 
             if(C1[0] > C2[0]) or (C1[1] > C2[1]):
+
                 messagebox.showerror("Error", "Las primeras coordenadas deben ser de la esquina superior izquierda")
+
             elif    ((C1[0] or C2[0]) > img.width) or ((C1[1] or C2[1]) > img.height):
+
                 messagebox.showerror("Error", "Las coordenadas deben ser menor al tamaño de la imagen")
+
             elif C1 == C2:
+
                 messagebox.showerror("Error", "Las coordenadas no pueden ser iguales")
+
             else:
                 cambiarPixeles(C1,C2)
 
@@ -47,7 +52,7 @@ def abrirImagen():
 
     ancho, alto = img.size
 
-    labelDimImg.config(text= str(ancho) + " x " + str(alto))
+    label_DimImg.config(text= str(ancho) + " x " + str(alto))
 
 
 def cambiarPixeles(C1,C2):
@@ -73,7 +78,6 @@ def mostrarImagen():
     panel_imagen.image = imagen_tk
 
 
-# Crear la ventana principal
 window = tk.Tk()
 window.title("Primer Programa PID")
 window.geometry("1000x1000")
@@ -85,21 +89,20 @@ filemenu = tk.Menu(menu)
 menu.add_cascade(label="Archivo", menu=filemenu)
 filemenu.add_command(label="Cargar imagen", command=abrirImagen)
 
-# Crear los frames
 frame_top = tk.Frame(window, height=500)
 
-frame_mid = tk.Frame(window, height=5)
+frame_mid = tk.Frame(window, height=1)
 
-labelDim = tk.Label(frame_mid, text="Dimensiones", font=("Consolas", 10))
-labelDim.pack()
+label_Dim = tk.Label(frame_mid, text="Dimensiones", font=("Consolas", 10))
+label_Dim.pack()
 
-labelDimImg = tk.Label(frame_mid, text="-", font=("Consolas", 10))
-labelDimImg.pack()
+label_DimImg = tk.Label(frame_mid, text="-", font=("Consolas", 10))
+label_DimImg.pack()
 
 frame_bottom = tk.Frame(window, bg="lightgreen", height=60)
 
-labelInstrucciones = tk.Label(frame_bottom, text="Ingrese las coordenadas de los puntos", font=("Consolas", 10))
-labelInstrucciones.grid(row=0, column=1, columnspan=4, padx=10, pady=10)
+label_Instrucciones = tk.Label(frame_bottom, text="Ingrese las coordenadas de las esquinas opuestas del rectangulo", font=("Consolas", 12))
+label_Instrucciones.grid(row=0, column=1, columnspan=4, padx=10, pady=10)
 
 
 labelX1 = tk.Label(frame_bottom, text="x1", font=("Consolas", 10))
@@ -126,16 +129,14 @@ labelY2.grid(row=2, column=3, padx=10, pady=10)
 entryY2 = tk.Entry(frame_bottom, font=("Consolas", 10))
 entryY2.grid(row=2, column=4, padx=10, pady=10)
 
-buttonIniciar = tk.Button(frame_bottom, text="Iniciar", font=("Consolas", 10), bg="green", fg="white", command=getCoordenadas)
-buttonIniciar.grid(row=2, column=5, columnspan=4, padx=10, pady=10)
+button_Iniciar = tk.Button(frame_bottom, text="Iniciar", font=("Consolas", 10), bg="green", fg="white", command=getCoordenadas)
+button_Iniciar.grid(row=2, column=5, columnspan=4, padx=10, pady=10)
 
 panel_imagen = tk.Label(frame_top)
 
-# Empacar los frames en la ventana
 frame_top.pack(fill=tk.BOTH, expand=True)
 frame_mid.pack(fill=tk.BOTH, expand=True)
 frame_bottom.pack(fill=tk.BOTH, expand=True)
-
 
 window.mainloop()
 
